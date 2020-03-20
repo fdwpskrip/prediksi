04652e0841fbd4a3bc963402dbbfd6f3d4be6a04
irvanwahyu <irvanyale14@gmail.com>
2020-03-20 08:48:10 +0700

    fixing layout

--- home/models.py
+++ home/models.py
@@ -5,9 +5,9 @@ class Data(models.Model):
     bulan = models.CharField("Bulan", max_length=50, blank=True, null=True)
     tahun = models.CharField("Tahun", max_length=50, blank=True, null=True)
     harga = models.CharField("Harga", max_length=50, blank=True, null=True)
-    produksi = models.CharField("Produksi", max_length=50, blank=True, null=True)
-    ketersediaan = models.CharField("Ketersediaan", max_length=50, blank=True, null=True)
-    permintaan = models.CharField("Permintaan", max_length=50, blank=True, null=True)
+    produksi = models.CharField("Produksi (KW)", max_length=50, blank=True, null=True)
+    ketersediaan = models.CharField("Ketersediaan (TON)", max_length=50, blank=True, null=True)
+    permintaan = models.CharField("Permintaan (TON)", max_length=50, blank=True, null=True)
     created_at = models.DateTimeField("Created At", auto_now_add=True)
 
     def __str__(self):
