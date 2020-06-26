b0b8fc791d97e0986cc7833bf3977dff9c026e6a
irvanwahyu <irvanyale14@gmail.com>
2020-06-26 10:06:30 +0700

    form

--- account/forms.py
+++ account/forms.py
@@ -13,18 +13,18 @@ STATUS_CHOICES = (
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(label='First Name', max_length=30,
-                                 widget=forms.TextInput())
-    last_name = forms.CharField(label='Last Name', max_length=150,
-                                widget=forms.TextInput())
-    username = forms.CharField(label='Username', max_length=50,
-                               widget=forms.TextInput())
-    email = forms.CharField(label='Email', max_length=50,
-                            widget=forms.TextInput())
-    password1 = forms.CharField(label='Password', max_length=30,
-                                widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Password Confirmation', max_length=30,
-                                widget=forms.PasswordInput())
+    first_name = forms.CharField(max_length=30,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=150,
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField(max_length=50,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.CharField(max_length=50,
+                            widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(max_length=30,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=30,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
     is_staff = forms.ChoiceField(label='Status', choices=STATUS_CHOICES)
 
     class Meta:
@@ -33,18 +33,18 @@ class SignUpForm(UserCreationForm):
 
 
 class SignUpFormUser(UserCreationForm):
-    first_name = forms.CharField(label='First Name', max_length=30,
-                                 widget=forms.TextInput())
-    last_name = forms.CharField(label='Last Name', max_length=150,
-                                widget=forms.TextInput())
-    username = forms.CharField(label='Username', max_length=50,
-                               widget=forms.TextInput())
-    email = forms.CharField(label='Email', max_length=50,
-                            widget=forms.TextInput())
-    password1 = forms.CharField(label='Password', max_length=30,
-                                widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Password Confirmation', max_length=30,
-                                widget=forms.PasswordInput())
+    first_name = forms.CharField(max_length=30,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=150,
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField(max_length=50,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.CharField(max_length=50,
+                            widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(max_length=30,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=30,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
 
     class Meta:
         model = User
@@ -52,10 +52,10 @@ class SignUpFormUser(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Username', max_length=20,
-                               widget=forms.TextInput())
-    password = forms.CharField(label='Password', max_length=20,
-                               widget=forms.PasswordInput())
+    username = forms.CharField(max_length=50,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(max_length=30,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
     class Meta:
         model = User
